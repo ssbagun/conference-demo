80181eee75c703763a8539a313b0b9edddd16c4e
ssbagun <ssbagun@gmail.com>
2021-03-04 00:48:24 +0900

    임시 커밋

---
+++ src/main/java/com/example/conferencedemo/ConferencedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferencedemoApplication.class, args);
+    }
+
+}
